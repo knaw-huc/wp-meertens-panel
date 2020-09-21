3726b871e6fd0c83c58413ece6c334979dee8b1c
Bas Doppen <basdoppen@gmail.com>
2020-09-21 09:51:20 +0200

    footer

--- _dist/meertensPanel/footer.php
+++ _dist/meertensPanel/footer.php
@@ -25,33 +25,7 @@
 
 </div><!-- #page -->
 <footer class="footer">
-  <div class="hcBasicSideMargin xCols">
 
-    <?php
-    if ( get_theme_mod('footer_block1') != '' ) {
-      echo '<div>'.get_theme_mod('footer_block1').'</div>';
-    }
-
-    if ( get_theme_mod('footer_block2') != '' ) {
-      echo '<div>'.get_theme_mod('footer_block2').'</div>';
-    }
-
-    if ( get_theme_mod('footer_block3') != '' ) {
-      echo '<div>'.get_theme_mod('footer_block3').'</div>';
-    }
-
-    //if ( get_theme_mod('footer_block4') != '' ) {
-      echo '<div class="footernav"><a href="/">Home</a>'.strip_tags($cleanMenu,"<a>").'</div>';
-    //}
-
-    if ( get_theme_mod('footerTwitterId') != '' ) {
-      echo '<div><a href="https://twitter.com/'.get_theme_mod('footerTwitterId').'"><img class="twitterIcon" src="'.get_bloginfo( 'template_url' ).'/images/icon-twitter.png">'.get_bloginfo( 'name' ).' on Twitter</a></div>';
-    }
-
-     ?>
-
-
-  </div>
 </footer>
 
 <?php wp_footer(); ?>
